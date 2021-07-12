5d0810680c3249ffb514fe86d168c3722aae5641
root <root@localhost.localdomain>
2021-07-12 17:48:54 +0300

    11011

--- frontend/src/routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from '@beyonk/sapper-rbac';
-
-const routes = new Router()
-  .unrestrict('/auth/*')
-  .restrict('.*', ['authenticated'])
-  .build();
-
-export default routes;
--- frontend/src/routes/settings.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>Settings</title>
-</svelte:head>
-
-<h1>Settings</h1>
-
-<p>This is the 'settings' page. There's not much here.</p>
