8950c3e23a882c69570a75e9233fe22ad199713b
root <root@localhost.localdomain>
2021-07-20 02:43:03 +0300

    order_exec 2

--- node-red-data/settings.js
+++ node-red-data/settings.js
@@ -116,14 +116,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2a$08$A3yXZN/Ri613MCywIGjaKONrQHpb2lYasV1Tu5.LAZ0JYOSpVykTK",
+            permissions: "*"
+        }]
+    },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
