c6840c5738c82b9b62bb8e18a083442b099682ff
root <root@localhost.localdomain>
2021-08-27 00:10:46 +0300

    front plus

---
+++ front/src/stores/statebot.js
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store";
+export const stateStore = writable({ rout: "botlist", 
+                                    showmenu: false,
+                                    selectbotname: "",
+                                    urlhost: "",
+                                    darkmodestatus: true,
+                                    timerId: "",
+                                    timerIdlist: ""});
